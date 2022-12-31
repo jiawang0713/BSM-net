9b7cad794f746ad404ba6a29951654a499cf4d12
jiawang0713 <33104038+jiawang0713@users.noreply.github.com>
2022-12-30 23:45:36 -0800

    Add files via upload

---
+++ real_data/citation_net/create_X.R
@@ -0,0 +1,49 @@
+#--------------------------------------------------
+#--- Variable Selection for High-dimensional Nodal Attributes 
+#--- in Social Networks with Degree Heterogeneity
+#--------------------------------------------------
+#--------- Real data analysis (Facebook friendship network)
+#----------   LDA on paper abstract to construct X
+#---------    last edited in July 12, 2021
+#--------------------------------------------------
+
+library(GGally)
+library(network)
+library(sna)
+setwd("~/Documents/Research/BVS_NET/citation_network/")
+load("citation_full.RData") # created in 'create_Y.R'
+set.seed(2020)
+
+selected_indx = read.csv(file = 'selected_indx_1207_n=476.csv')
+selected_indx = as.vector(selected_indx$x)
+selected_indx_sub = match(selected_indx, indx_sub)
+
+# select more documents to build LAD
+sample_ind = sample( (1:length(indx_sub))[-selected_indx_sub] , 5000-n)
+sample_ind = c(selected_indx_sub, sample_ind)
+
+# preprocess 
+library(tm)
+corpus<-abstract[indx_sub][sample_ind]
+docs <- Corpus(VectorSource(unlist(corpus)))
+docs = DocumentTermMatrix(docs, control = list(stemming = TRUE, stopwords = stopwords("english") , minWordLength = 3, removeNumbers = TRUE, removePunctuation = TRUE))
+
+library("slam")
+summary(col_sums(docs))
+# term frequency-inverse document frequency
+term_tfidf = tapply(docs$v/row_sums(docs)[docs$i], docs$j, mean) *log2(nDocs(docs)/col_sums(docs > 0))
+docs <- docs[, term_tfidf >= 0.1]
+ind_deleted = which(row_sums(docs) ==0)
+docs<- docs[row_sums(docs) > 0,]
+summary(col_sums(docs))
+dim(docs)
+
+library("topicmodels")
+k = 500 # the number of topics
+SEED = 0713
+LDA.Gibbs.fit =  LDA(docs, k = k, method = "Gibbs", control = list(seed = SEED, burnin = 2000, thin = 100, iter = 1000))
+
+Terms <- terms(LDA.Gibbs.fit , 5) # The 5 most frequent terms for each topics for interpretation. 
+
+write.csv(Terms,file = '~/Documents/Research/BVS_NET/citation_network_V9/Terms_1207_n=476.csv')
+write.csv(posterior(LDA.Gibbs.fit)$topic[1:n,],file = '~/Documents/Research/BVS_NET/citation_network_V9/citation_X_1207_n=476.csv')
---
+++ real_data/citation_net/create_Y.R
@@ -0,0 +1,98 @@
+#--------------------------------------------------
+#--- Variable Selection for High-dimensional Nodal Attributes 
+#--- in Social Networks with Degree Heterogeneity
+#--------------------------------------------------
+#--------- Real data analysis (Facebook friendship network)
+#----------   Construct the adjacency matrix Y based on citation data set https://www.aminer.cn/citation
+#---------    last edited in July 22, 2021
+#--------------------------------------------------
+
+#--------------------------------------------------
+#--------- Step 1: convert raw text data 'acm.txt' into 'citation_full.RData' (file contained in the folder)
+#--------------------------------------------------
+
+con = file("~/Documents/Research/BVS_NET/citation_network_v9/acm.txt", "r") # 'acm.txt' could be downloaded from https://www.aminer.cn/citation Citation-network V1
+num=2385022 
+title<-rep("", num)
+author<-rep("", num)
+year<-rep(0, num)
+publication<-rep("", num)
+citation<-rep("", num)
+year<-rep(0, num)
+index<-rep(0, num)
+abstract<-rep("", num)
+
+count=1
+while (count<(num+1)) {
+  line = readLines(con, n = 1)
+  if(line ==""){
+    if(citation[count]!="")
+    {
+      citation[count]=substring(citation[count], 2)
+    }
+    count=count+1
+    
+  } else if(substring(line, 1,2)=="#*") 
+  {
+    title[count]=substring(line, 3)
+  } else if(substring(line, 1,2)=="#@") {
+    author[count]=substring(line, 3)
+  } else if(substring(line, 1,2)=="#t") {
+    year[count]=as.numeric(substring(line, 3))
+  } else if(substring(line, 1,2)=="#c") {
+    publication[count]=substring(line, 3)
+  } else if(substring(line, 1,2)=="#%") {
+    citation[count]=paste(citation[count],substring(line, 3), sep=" ")
+  } else if(substring(line, 1,6)=="#index") {
+    index[count]=as.numeric(substring(line, 7))
+  } else if(substring(line, 1,2)=="#!") {
+    abstract[count]=substring(line, 3)
+  } 
+}
+close(con)
+save(title, author, year, publication, citation, index, abstract,file = "~/Documents/Research/BVS_NET/citation_network_v9/citation_full.RData")
+
+#--------------------------------------------------
+#--------- Step 2: Get node degree for each paper and save it in a vector named 'count' (file contained in the folder)
+#--------------------------------------------------
+
+indx_sub = which(abstract!="")
+temp = which(citation!="")
+indx_sub = intersect(indx_sub,temp)
+current_indx = indx_sub
+current_n = length(current_indx)
+current_edge_list = citation[current_indx]
+
+# get degree for each node, save in a vector named 'count'
+count = rep(0,current_n)
+for (j in 1:current_n){
+  temp =  as.numeric(unlist(strsplit(current_edge_list[j], split=" "))) + 1 
+  count[j] = count[j]+length(temp)  
+  ind = match(temp, current_indx)
+  ind = ind[!is.na(ind)]
+  if (length(ind)>0){count[ind] = count[ind] +1 }
+}
+write.csv(count,file = '~/Documents/Research/BVS_NET/citation_network_V9/count.csv')
+
+#--------------------------------------------------
+#--------- Step 3: Get a dense sub-network with samller size 
+#--------------------------------------------------
+
+count = read.csv("~/Documents/Research/BVS_NET/citation_network_V9/count.csv")
+count = count$x
+ind = which(count >200)
+selected_indx = current_indx[ind]
+selected_edge_list = current_edge_list[ind]
+Y = matrix(0, ncol= length(selected_indx), nrow= length(selected_indx))
+for (i in 1:length(selected_edge_list)){
+  temp<- as.numeric(unlist(strsplit(as.vector(selected_edge_list[i]), split=" "))) + 1 
+  temp = intersect(temp, selected_indx)
+  if (length(temp)>0){
+    ind = match(temp, selected_indx)
+    Y[i, ind] = rep(1, length(ind))
+    Y[ind, i ] = rep(1, length(ind))
+  }
+}
+n = length(selected_indx)
+write.csv(Y,file = '~/Documents/Research/BVS_NET/citation_network_V9/citation_Y_1207_n=476.csv')
+write.csv(selected_indx,file = '~/Documents/Research/BVS_NET/citation_network_v9/selected_indx_1207_n=476.csv')
